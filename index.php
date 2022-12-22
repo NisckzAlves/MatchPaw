a860d99249c81f552bcc6f5722dce7598e61b415
Nicole <118059749+NisckzAlves@users.noreply.github.com>
2022-12-22 07:56:33 -0300

    add login error

--- index.php
+++ index.php
@@ -1,6 +1,13 @@
 <!DOCTYPE html>
 <html lang="en">
+
+
 <head>
+<meta charset="UTF-8">
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+  <script src="https://code.jquery.com/jquery-3.6.1.min.js" integrity="sha256-o88AwQnZB+VDvE9tvIXrMQaPlFFSUTR+nldQm1LuPXQ=" crossorigin="anonymous"></script>
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link rel="shortcut icon" href="favicon.ico" />
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
